9d26822ddcd7724da6d53cfc6701b85907b636e2
Arnaud <arnaud.cabrolier@gmail.com>
2026-03-13 12:53:53 +0100

    ignoring 2 classes

--- src/test/java/suite2/SauceDemoTests.java
+++ src/test/java/suite2/SauceDemoTests.java
@@ -18,83 +18,83 @@ import java.time.Duration;
 
 
 public class SauceDemoTests {
-    private WebDriver driver;
-    private String url = "https://www.saucedemo.com/";
-    private String header = "Swag Labs";
-
-    //For demonstrating parameterized builds
-    String browser = System.getProperty("browser");
-
-
-    public void waitForPageLoaded() {
-        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver driver) {
-                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
-            }
-        };
-        try {
-            Thread.sleep(1000);
-            int d = 30; //duration
-            Duration du = Duration.ofMillis(30);
-            WebDriverWait wait = new WebDriverWait(driver, du); //ok
-            wait.until(expectation);
-        } catch (Throwable error) {
-            Assert.fail("Timeout waiting for Page Load Request to complete.");
-        }
-    }
-
-    //Test to launch browser with url
-    @Test(enabled = true, priority = 0)
-    public void open() {
-        driver.get(url);
-        waitForPageLoaded();
-        String title = driver.getTitle();
-        System.out.println(title);
-        //My Store
-        AssertJUnit.assertTrue(title.equals(header ));
-    }
-
-    //Login
-
-    @Test(enabled = true, priority = 1)
-    public void login() {
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.className("btn_action")).click();
-        AssertJUnit.assertTrue(driver.findElement(By.className("product_label")).getText()
-                .equals("Products"));
-    }
-
-    //Before test
-    @BeforeTest
-    public void beforeTest() {
-        //Instantiate browser based on user input
-
-        if(browser != "" && browser != null) {
-            if(browser.equalsIgnoreCase("Chrome")) {
-                driver = new ChromeDriver();
-                driver.manage().window().maximize();
-            }
-            else if(browser.equalsIgnoreCase("firefox")) {
-                driver = new FirefoxDriver();
-                driver.manage().window().maximize();
-            }
-            else {
-                System.out.println("Invalid option Selected hence defaulting to Chrome");
-                browser = "Chrome";
-                driver = new ChromeDriver();
-                driver.manage().window().maximize();
-            }
-        }
-        else {
-            browser = "Chrome";
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-        }
-    }
-    //hooks - to tear down after test is executed
-    @AfterTest
-    public void afterTest() {
-        driver.quit();
-    }
+//    private WebDriver driver;
+//    private String url = "https://www.saucedemo.com/";
+//    private String header = "Swag Labs";
+//
+//    //For demonstrating parameterized builds
+//    String browser = System.getProperty("browser");
+//
+//
+//    public void waitForPageLoaded() {
+//        ExpectedCondition<Boolean> expectation = new ExpectedCondition<Boolean>() {
+//            public Boolean apply(WebDriver driver) {
+//                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+//            }
+//        };
+//        try {
+//            Thread.sleep(1000);
+//            int d = 30; //duration
+//            Duration du = Duration.ofMillis(30);
+//            WebDriverWait wait = new WebDriverWait(driver, du); //ok
+//            wait.until(expectation);
+//        } catch (Throwable error) {
+//            Assert.fail("Timeout waiting for Page Load Request to complete.");
+//        }
+//    }
+//
+//    //Test to launch browser with url
+//    @Test(enabled = true, priority = 0)
+//    public void open() {
+//        driver.get(url);
+//        waitForPageLoaded();
+//        String title = driver.getTitle();
+//        System.out.println(title);
+//        //My Store
+//        AssertJUnit.assertTrue(title.equals(header ));
+//    }
+//
+//    //Login
+//
+//    @Test(enabled = true, priority = 1)
+//    public void login() {
+//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//        driver.findElement(By.className("btn_action")).click();
+//        AssertJUnit.assertTrue(driver.findElement(By.className("product_label")).getText()
+//                .equals("Products"));
+//    }
+//
+//    //Before test
+//    @BeforeTest
+//    public void beforeTest() {
+//        //Instantiate browser based on user input
+//
+//        if(browser != "" && browser != null) {
+//            if(browser.equalsIgnoreCase("Chrome")) {
+//                driver = new ChromeDriver();
+//                driver.manage().window().maximize();
+//            }
+//            else if(browser.equalsIgnoreCase("firefox")) {
+//                driver = new FirefoxDriver();
+//                driver.manage().window().maximize();
+//            }
+//            else {
+//                System.out.println("Invalid option Selected hence defaulting to Chrome");
+//                browser = "Chrome";
+//                driver = new ChromeDriver();
+//                driver.manage().window().maximize();
+//            }
+//        }
+//        else {
+//            browser = "Chrome";
+//            driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+//        }
+//    }
+//    //hooks - to tear down after test is executed
+//    @AfterTest
+//    public void afterTest() {
+//        driver.quit();
+//    }
 }
